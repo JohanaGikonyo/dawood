ffe1314c9ca63cdd9f70e863571d5041c306c705
JohanaGikonyo <johanagikonyo552@gmail.com>
2025-01-12 00:29:34 +0300

    params

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Section1Intro from "./Section1Intro";
 import Section2Items from "./Section2Items";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 function Main() {
-  const searchParams = useSearchParams();
+  const [category, setCategory] = useState<string>("ready"); // Default category
   const router = useRouter();
-  const category = searchParams.get("category");
+
+  // Get the current category from the URL
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const categoryParam = params.get("category");
+    if (categoryParam) {
+      setCategory(categoryParam);
+    }
+  }, [window.location.search]); // Re-run when the URL search params change
 
   // Function to handle navigation
-  const handleNavigation = (category: string) => {
+  const handleNavigation = (category) => {
+    setCategory(category);
     router.push(`/products?category=${category}`);
   };
+
   return (
     <div>
       <Section1Intro />
-      <nav className="sticky top-0 bg-gradient-to-r from-teal-100 to-teal-50  overflow-x-auto no-scrollbar z-10 px-5 pr-14 ">
+      <nav className="sticky top-0 bg-gradient-to-r from-teal-100 to-teal-50 overflow-x-auto no-scrollbar z-10 px-5 pr-14">
         <div className="flex justify-around lg:justify-center py-4 gap-5">
           {[
             { label: "Ready", value: "ready" },
@@ -31,7 +41,7 @@ function Main() {
               className={`flex flex-col items-center gap-1 px-2 py-1 rounded-xl font-medium text-sm ${
                 category === item.value
                   ? "bg-teal-400 text-white"
-                  : " border-2 border-teal-200 text-teal-500 hover:bg-teal-400 hover:text-white"
+                  : "border-2 border-teal-200 text-teal-500 hover:bg-teal-400 hover:text-white"
               } transition`}
             >
               {item.label}
@@ -39,7 +49,7 @@ function Main() {
           ))}
         </div>
       </nav>
-      <Section2Items />
+      <Section2Items category={category} />
     </div>
   );
 }
